74fff6829603552939d2f265ee5f258347d0e34d
adcva <onackirema@gmail.com>
2023-04-13 21:18:50 +0300

    Changes

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -5,6 +5,7 @@ import { RiDeleteBack2Line } from "react-icons/ri";
 
 function AddTask() {
     const [showAddTaskModal, setShowAddTaskModal] = useState(true);
+    const [subtasks, setSubtasks] = useState([]);
 
     const transition = useTransition(showAddTaskModal, {
         from: { opacity: 0 },
@@ -23,18 +24,35 @@ function AddTask() {
                         <div className='form-group'>
                             <label>Title</label>
                             <input type='text' placeholder='e.g. Take coffe break'/>
+                            <p>This fiels is required.</p>
                         </div>
 
                         <div className='form-group'>
-
+                            <label>Description</label>
+                            <textarea placeholder="e.g. It's always good to take a break. This 15 minutes break will recharge the batteries a little."></textarea>
+                            <p>This fiels is required.</p>
                         </div>
 
                         <div className='subtasks-container'>
-
+                            <label>Subtasks</label>
+                            {subtasks ? subtasks.map((el, i) => {
+                                return (
+                                    <div key={i}>
+                                        <input type='text' name='subtaskName' value={el.subtaskName} placeholder={placeholder[i]} />
+                                        <button type='button'><RiDeleteBack2Line /></button>
+                                    </div>
+                                )
+                            }) : null}
+                            <button type='button' onClick={addSubTaskInput}>+ Add New Subtask</button>
                         </div>
 
                         <div className='form-group'>
-
+                            <label>Status</label>
+                            <select name="status">
+                                <option value="todo" selected>Todo</option>
+                                <option value="doing">Doing</option>
+                                <option value="done">Done</option>
+                            </select>
                         </div>
 
                         <button type='submit'>Create Task</button>
